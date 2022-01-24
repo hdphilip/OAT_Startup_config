1828e0c55c49a347d36f85a253a01f6abed7b1d8
Philip Crawford <hdphilip@yahoo.com>
2022-01-23 19:17:16 -0800

    new raw files
    make sure mpu6050 and oat test are in the same directory.

---
+++ mpu6050.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+
+# cd start/python3 mpu3.py
+
+
+import smbus
+import math
+import time
+
+
+
+# Power management registers
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+REG_CONFIG   = 0x1A #  5Hz bandwidth (lowest) for smoothing
+REG_ACCEL_XOUT_H = 0x3B
+REG_TEMP_OUT_H   = 0x41
+REG_PWR_MGMT_1   = 0x6B
+REG_WHO_AM_I     = 0x75
+ 
+def read_byte(adr):
+    return bus.read_byte_data(address, adr)
+ 
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+ 
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+ 
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+ 
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+ 
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+ 
+ 
+bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68 # This is the address value read via the i2cdetect command
+bus.write_byte_data(address, REG_PWR_MGMT_1 , 0)  # Now wake the 6050 up as it starts in sleep mode
+bus.write_byte_data(address, REG_CONFIG , 6)      # Reduce BW to 5HZ 
+ 
+
+
+accel_xout = read_word_2c(0x3b)
+accel_yout = read_word_2c(0x3d)
+accel_zout = read_word_2c(0x3f)
+
+accel_xout_scaled = accel_xout / 4096   # 16384.0 8192
+accel_yout_scaled = accel_yout / 4096
+accel_zout_scaled = accel_zout / 4096
+
+Xang = get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+Yang = get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+
+Xang = Xang - .2  # RA calibration error
+Yang = Yang + 2.3  # RA calibration error
+
+Xa = "%.1f" %Xang
+Ya = "%.1f" %Yang
+   
+ 
+   # print ("Xangle=%.1f" %Xangle , "Yangle=%.1f" %Yangle)
+print (Xa,Ya)
+  
+#time.sleep(0.25)
+
+
+
+
---
+++ oat_test_1_0.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import serial
+import sys, importlib
+from mpu6050 import Xa
+from mpu6050 import Ya
+
+target_DEC = 40
+target_RA  = 0
+DEC_steps_per_degree = 314.2
+RA_steps_per_degree = 314.2
+#steps = 1.2
+#Ya = 13.0
+
+print("COM port set to /dev/ttyUSB0")
+print("Disabling #DTR for /dev/ttyUSB0")
+serialport = "/dev/ttyUSB0"
+os.system('stty -F /dev/ttyUSB0 -hupcl')
+print("Opening serial port on " + serialport + '...')
+ser = serial.Serial(serialport, 19200, timeout = 1)
+time.sleep(1)#wait for reboot
+
+
+#//////////////////////////
+# RA Alignment
+#//////////////////////////
+
+importlib.reload(sys.modules.get('mpu6050', sys))
+
+Xa = float(Xa)  # convert int to float 
+
+RA_steps = ((target_RA - Xa) * RA_steps_per_degree )
+RA_error = target_RA - Xa
+RA_error = "%.1f" %RA_error # Cleans up the display
+RA_steps = "%.1f" %RA_steps # Cleans up the display
+
+print ("\n Current RA angle Before Adjustment " +str(Xa) + " Degrees")
+print ("\n Target RA angle - Current angle " + str(RA_error) + "   Degrees" )
+print ("\n Steps per Degrees  " + str(RA_steps_per_degree))
+print ("\n Steps to reach Goal " + str(RA_steps))
+
+move_RA_stepper = (':MXr' + str(RA_steps) + '#') # configures the Measde command
+
+ser.write(str.encode(move_RA_stepper))
+response = ser.read(1)
+response_utf = (response.decode('utf-8'))
+if int(response_utf) == 1:
+    print('\n RA successfully Homed!')
+else: print('\n Could not Home RA...')
+time.sleep(7)  # wait for Dec to complete it's move
+
+importlib.reload(sys.modules.get('mpu6050', sys))  # Display new RA and DEC values
+
+
+#//////////////////////////
+# Dec Alignment
+#//////////////////////////
+
+importlib.reload(sys.modules.get('mpu6050', sys))
+
+Ya = float(Ya)  # convert int to float 
+
+dec_steps = ((target_DEC - Ya) * DEC_steps_per_degree )
+dec_error = target_DEC - Ya
+dec_error = "%.1f" %dec_error # Cleans up the display
+dec_steps = "%.1f" %dec_steps # Cleans up the display
+
+print ("\n Current Dec angle Before Adjustment " +str(Ya) + " Degrees")
+print ("\n Target Dec angle - Current angle " + str(dec_error) + "   Degrees" )
+print ("\n Steps per Degrees  " + str(DEC_steps_per_degree))
+print ("\n Steps to reach Goal " + str(dec_steps))
+
+move_dec_stepper = (':MXd' + str(dec_steps) + '#')
+
+ser.write(str.encode(move_dec_stepper))
+response = ser.read(1)
+response_utf = (response.decode('utf-8'))
+if int(response_utf) == 1:
+    print('\n DEC successfully Homed!')
+else: print('\n Could not Home DEC...')
+time.sleep(15)  # wait for Dec to complete it's move
+
+importlib.reload(sys.modules.get('mpu6050', sys))  # Display new RA and DEC values
+
+#///////////////////////////////////
+#  Turn Tracking Off
+#///////////////////////////////////
+time.sleep(5)  #wait for reboot
+ser.write(str.encode(':MT0#'))
+response = ser.readline()
+response_utf = (response.decode('utf-8'))
+if (response_utf) == ('1') :
+    print('\n OAT Tracking should be OFF...')
+else:  print(str(response_utf) + 'Problems with Turning Tracking off')
+
+
+#////////////////////////////////////
+# Stop and exit
+#///////////////////////////////////
+print('\n Stoping all motors...')
+ser.write(str.encode(':Q#'))
+
+ser.close() 
+
+print("/// OAT configuration is Completed ///")
+
+
+
+
+
+
